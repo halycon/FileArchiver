6281c70fe929662827d4dbe878e406a12adecaa5
volkancetin <vncetin@gmail.com>
2018-06-16 23:16:21 +0300

    mysql ddl directory added , README.md file modified

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# FileArchiver
+## FileArchiver
+This service application project act as middleware for IBM FiletNet repository.
+
+## Code style
+If you're using any code style like xo, standard etc. That will help others while contributing to your project. Ex. -
+
+[![eclipse-cd](https://img.shields.io/badge/code%20style-standard-brightgreen.svg?style=flat)](https://github.com/checkstyle/eclipse-cs)
+ 
+
+## Tech/framework used
+Spring Boot , Jax-WS, RabbitMQ , MySQL
+Swagger for Documentation
+
+<b>Built with</b>
+- [Gradle](https://gradle.org)
+
+## Installation
+
+RabbitMQ have to be up installed and running with either package manager , manually or up with a docker image
+MySQL have to be up installed and running with either package manager , manually or up with a docker image 
+(database related info in mysql_schema_ddl directory)
+Gradle have to be installed
+
+gradle build
+java -Dserver.port=8294 -Djava.security.egd=file:/dev/./urandom -Denvironment=devel -jar build/libs/FileArchiver.jar
+
+## API Reference
+
+Swagger UI
+http://localhost:9294/swagger-ui.html#/
+
+Soap WSDL
+http://localhost:9294/FileArchiverSoap?wsdl
+
+
+© [Volkan Cetin]()
---
+++ mysql_schema_ddl/arkheion_channel.sql
@@ -0,0 +1,49 @@
+-- MySQL dump 10.13  Distrib 5.7.9, for osx10.9 (x86_64)
+--
+-- Host: 10.0.106.41    Database: arkheion
+-- ------------------------------------------------------
+-- Server version	5.7.20
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `channel`
+--
+
+DROP TABLE IF EXISTS `channel`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `channel` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(250) DEFAULT NULL,
+  `profile_id` int(11) NOT NULL,
+  `create_date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `implement_id` int(11) NOT NULL,
+  `status` int(11) DEFAULT '1',
+  PRIMARY KEY (`id`),
+  KEY `profile_id_idx` (`profile_id`),
+  KEY `implement_id_idx` (`implement_id`),
+  CONSTRAINT `implement_id` FOREIGN KEY (`implement_id`) REFERENCES `implementation` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `profile_id` FOREIGN KEY (`profile_id`) REFERENCES `profile` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-06-16 23:02:20
